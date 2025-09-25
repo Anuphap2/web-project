d0bc42bc355862a9e4afa4b36a64db91eb0cef7b
Anuphap2 <anuphap.240348@gmail.com>
2025-09-25 09:25:44 +0700

    first commit

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full border-b bg-white dark:border-neutral-800 dark:bg-black">
+      <div className="mx-auto max-w-7xl py-4 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100">
+          My Website
+        </h1>
+      </div>
+    </nav>
+  );
+}
