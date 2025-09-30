99abae46c7bf0f7f278ec7779f44a42818ae0868
Anuphap2 <anuphap.240348@gmail.com>
2025-09-30 20:02:38 +0700

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,56 @@
-// App.js
-import React from "react";
-
-// โค้ดสำหรับแสดงผลใน React
+import Image from "next/image";
 export default function App() {
   return (
     <>
-      <div className="hero min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col min-h-screen">
-        <div className="relative hero-2 flex-1 flex items-center justify-center">
-          {/* Hero content for the top section */}
-          <div className="hero-content text-center text-white py-20">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
+      <main className="min-h-screen bg-landing">
+        {/* Hero section */}
+        <div className="hero min-h-screen">
+          <div className="hero-content flex justify-between">
+            <div className="text-center lg:text-left mt-[425px]">
+              <h1 className="text-5xl font-bold">About</h1>
               <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                WorkFlow Manager is a task management system that makes work
+                clear and efficient. Managers can assign tasks and view
+                employees, while employees can access departmental tasks, accept
+                them, or work on assigned ones — all with real-time tracking and
+                improved collaboration.
               </p>
-              <button className="btn btn-primary">Get Started</button>
+            </div>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <div className="bg-gray-200 p-8">
+                  <h1 className="tracking-[0.4em] text-4xl font-bold mb-[1rem]">
+                    TASKS
+                  </h1>
+                  <h2 className="tracking-[0.4em] text-2xl font-bold mt-[-1.5rem]">
+                    FLOW
+                  </h2>
+
+                  <div>
+                    Task management that flows smoothly, clearly, and
+                    collaboratively.
+                  </div>
+
+                  <div className="mt-[2rem] w-full">
+                    <div className="flex justify-center">
+                      <button className="btn bg-black text-white rounded-[50px] w-2/3 p-6 hover:bg-secondary">
+                        Login
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <Image
+                  src="/qr.jpg"
+                  width={274.29 / 2}
+                  height={66 / 2}
+                  alt=""
+                ></Image>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="flex flex-col min-h-screen">
-        <div className="relative hero-3 flex-1 flex items-center justify-center">
-          {/* Hero content for the top section */}
-          <div className="hero-content text-center text-white py-20">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col min-h-screen">
-        <div className="relative hero-4 flex-1 flex items-center justify-center">
-          {/* Hero content for the top section */}
-          <div className="hero-content text-center text-white py-20">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      </main>
     </>
   );
 }
