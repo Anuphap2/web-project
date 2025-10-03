48310f059d8437329b750c7e83b8c7b42217f34e
Anuphap2 <anuphap.240348@gmail.com>
2025-10-03 21:09:43 +0700

    update layout page

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Layout/Navbar";
+import Footer from "@/components/Layout/Footer";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </>
+  );
+}
